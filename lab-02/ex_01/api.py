5a9e5f9a198b73f4594a5dbc4b167c60e7285a98
mrThenh2810 <mrThenh2810@gmail.com>
2025-03-13 13:18:55 +0700

    thanh

--- lab-02/ex_01/api.py
+++ lab-02/ex_01/api.py
@@ -9,3 +9,5 @@ caesar_cipher = CaesarCipher()
 def caesar_encrypt():
     data = request.json 
     plain_text = data['plain_text']
+    key = int(data['key'])
+    encrypted_text =  caesar_cipher.encrypt_text
