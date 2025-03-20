027ce63851fbfa65352b75148a043c4368d292e7
DuyThenh28 <MrThenh2810@gmail.com>
2025-03-20 13:51:51 +0700

    aa

--- lab-02/ex_01/api.py
+++ lab-02/ex_01/api.py
@@ -1,3 +1,5 @@
+from cipher.railfence import RailFenceCipher
+from cipher.transposition import TranspositionCipher
 from flask import Flask, request, jsonify
 from cipher.caesar import CaesarCipher
 from cipher.vigenere import VigenereCipher
@@ -39,6 +41,25 @@ def vigenere_decrypt():
     key = (data['key'])
     decrypted_text =  vigenere_cipher.vigenere_decrypt(cipher_text, key)
     return jsonify ({'encrypted_text': decrypted_text})
+
+transposition_cipher = TranspositionCipher()
+
+@app.route('/api/transposition/encrypt', methods=['POST'])
+def transposition_encrypt():
+    data = request.get_json()
+    plain_text = data.get('plain_text')
+    key = int(data.get('key'))
+    encrypted_text = transposition_cipher.encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/transposition/decrypt', methods=['POST'])
+def transposition_decrypt():
+    data = request.get_json()
+    cipher_text = data.get('cipher_text')
+    key = int(data.get('key'))
+    decrypted_text = transposition_cipher.decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
+
 
 
 #main funtion
